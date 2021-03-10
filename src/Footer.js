722b30a03ef8102214fb79a03c22a279924e0c7c
Nitesh Sharma <niteshsharma312000@gmail.com>
2021-03-10 22:41:49 +0530

    Footer improved

--- src/Footer.js
+++ src/Footer.js
@@ -8,18 +8,24 @@ import RepeatIcon from '@material-ui/icons/Repeat'
 import { Grid, Slider } from '@material-ui/core'
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay'
 import VolumeDownIcon from '@material-ui/icons/VolumeDown'
+import { useDataLayerValue } from './DataLayer'
 
 function Footer () {
+
+    const [{discover_weekly}, dispatch] = useDataLayerValue()
+
+    console.log(JSON.stringify(discover_weekly))
+
     return (
         <div className="footer">
             <div className="footer__left">
                 <img 
                     className="footer__albumLogo"
-                    src="https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Got7_-_Flight_Log_Departure.jpg/220px-Got7_-_Flight_Log_Departure.jpg" 
+                    src={discover_weekly?.tracks.items[5].track.album.images[0].url}
                     alt="" />
                 <div className="footer__songInfo">
-                    <h4>Yeah!</h4>
-                    <p>Usher</p>
+                    <h4>{discover_weekly?.tracks.items[5].track.name}</h4>
+                    <p>{discover_weekly?.tracks.items[5].track.artists.map((artist) => artist.name).join(",")}</p>
                 </div>
             </div>
 
